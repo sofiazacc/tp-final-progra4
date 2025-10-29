a5276c6a1c02f50184e3db65afbf75a305f4b3d3
sofi <sofiazaccagnini04@gmail.com>
2025-10-28 22:27:42 -0300

    componentes base de la aplicación

---
+++ src/app/guards/guards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guards',
+  imports: [],
+  templateUrl: './guards.html',
+  styleUrl: './guards.css'
+})
+export class Guards {
+
+}
