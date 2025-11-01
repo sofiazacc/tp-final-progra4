5490907d35e61e9e81be62763102660d0689c582
sofi <sofiazaccagnini04@gmail.com>
2025-11-01 03:05:44 -0300

    Se creó el grupo para el formulario de login y el método correspondiente para enviarlo

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
-
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
+
 export class Login {
-  
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    contra: new FormControl('', [Validators.required, Validators.minLength(8)])
+  })
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log("Formulario enviado correctamente", this.loginForm.value);
+    } else {
+      console.log("Formulario no válido");
+    }
+  }
 }
