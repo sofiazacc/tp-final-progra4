eb5b33a9f69407165a6fe212e440fe9706315d23
sofi <sofiazaccagnini04@gmail.com>
2025-11-01 19:52:02 -0300

    Se definieron los métodos getProvincias y getLocalidaded en el servicio que se comunica con la API GeoRef

--- src/app/services/georer.spec.ts
+++ src/app/services/georer.spec.ts
@@ -1,8 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Georer } from './georer';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
 
 describe('Georer', () => {
   let service: Georer;
