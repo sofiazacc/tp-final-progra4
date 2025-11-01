705f65d52dd170ead6d75568e8d5f95cef00e07e
sofi <sofiazaccagnini04@gmail.com>
2025-11-01 04:20:16 -0300

    Traducción del html del login/registro que ya teníamos hecho previamente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
+import { Auth } from './pages/auth/auth';
 import { Home } from './pages/home/home';
 import { Feed } from './pages/feed/feed'; 
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full' },
-    {path: 'auth', component: Login },
+    {path: 'auth', component: Auth },
     {path: 'home', component: Home },
     {path: 'feed', component: Feed, canActivate: [authGuard] }
 ];
--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 @Component({
-  selector: 'app-login',
-  imports: [ReactiveFormsModule],
-  templateUrl: './login.html',
-  styleUrl: './login.css'
+  selector: 'app-auth',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './auth.html',
+  styleUrl: './auth.css'
 })
 
-export class Login {
+export class Auth {
+
+  esLogin = true
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     contra: new FormControl('', [Validators.required, Validators.minLength(8)])
@@ -18,6 +22,7 @@ export class Login {
   registroForm = new FormGroup({
     nombre: new FormControl('', [Validators.required]),
     apellido: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
     ciudadDeOrigen: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     contra: new FormControl('', [Validators.required, Validators.minLength(8)])
