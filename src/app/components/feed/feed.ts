cc05e414ec479c7679b621927be79aaa9fd82479
Nicolas <nico.k2715@gmail.com>
2025-11-04 04:14:04 -0300

    Se creó y desarrolló el componente feed

---
+++ src/app/components/feed/feed.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FeedService } from '../../services/feed-service';
+import { PostComponent } from '../post/post';
+
+@Component({
+  selector: 'app-feed',
+  standalone: true,  
+  imports: [PostComponent],
+  templateUrl: './feed.html',
+  styleUrls: ['./feed.css']
+})
+
+export class Feed implements OnInit {
+
+  constructor(public feedService: FeedService) {}
+
+  ngOnInit(): void {
+    console.log("Feed iniciado");
+    this.obtenerPosts();
+  }
+  
+  obtenerPosts() {
+    console.log("Ejecutando obtenerPosts()"); 
+    this.feedService.getPosts().subscribe({
+      next: (data) => { 
+        console.log("Datos recibidos:", data);
+        this.feedService.posts = data },
+      error: (e) => console.log(e)
+    });
+  }
+}
