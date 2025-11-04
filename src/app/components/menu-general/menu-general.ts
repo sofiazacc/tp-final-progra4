05a4b93386fe5843b78314f6da7abff21fc17aaa
Nicolas <nico.k2715@gmail.com>
2025-11-04 01:16:47 -0300

    Se creó la clase. Se creó también un array de objetos (botones) y un método que cambia de estado (desplegarContraer)

---
+++ src/app/components/menu-general/menu-general.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+  selector: 'app-menu-general',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './menu-general.html',
+  styleUrl: './menu-general.css'
+})
+export class MenuGeneral {
+
+  desplegado = true;
+
+  desplegarContraer(){
+    this.desplegado = !this.desplegado
+  }
+
+  menuItems = [
+    {icon: 'dynamic_feed', label: 'Feed', route: ''},
+    {icon: 'map_search', label: 'Mapa', route: ''},
+    {icon: 'person', label: 'Perfil', route: ''},
+    {icon: 'bookmark', label: 'Posts guardados', route: ''},
+    {icon: 'account_circle_off', label: 'Cerrar sesión', route: ''},
+  ]
+
+}
