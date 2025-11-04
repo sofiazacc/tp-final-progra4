2afe9519df0cc4cb2082197a51e2e0f91022fd6e
Nicolas <nico.k2715@gmail.com>
2025-11-04 01:13:47 -0300

    Se gregó MenuGeneral al array de imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuGeneral } from './components/menu-general/menu-general';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuGeneral],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
