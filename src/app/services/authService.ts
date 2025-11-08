5325193c5cc7e149640d22a9edb06e95f0ab6863
sofi <sofiazaccagnini04@gmail.com>
2025-11-08 19:33:14 -0300

    Se creó el método de registro y otros métodos relacionados al token

--- src/app/services/authService.ts
+++ src/app/services/authService.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Fotografo } from '../models/usuario';
+import { tap } from 'rxjs';
 import { Usuario } from '../models/usuario';
 
 @Injectable({
@@ -9,17 +9,51 @@ import { Usuario } from '../models/usuario';
 })
 
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/usuarios';
+  private backURL = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(`${this.apiUrl}?email=${email}&password=${password}&rol=fotografo`);
+  login(credenciales: any): Observable<{accesToken: string, user: Usuario}> {
+    return this.http.post<{accesToken: string, user: Usuario}>(`${this.backURL}/login`, credenciales).pipe(
+      tap(response => {
+        this.guardarToken(response.accesToken);
+        this.guardarUsuario(response.user)
+      })
+    );
   } //El login es para cualquier tipo de usuario
 
   register(fotografo: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, fotografo);
+    return this.http.post(`${this.backURL}/register`, fotografo);
   } //El registro es solo para fotógrafos
 
+   guardarToken(token: string): void { 
+   localStorage.setItem('token_jwt', token);
+  }
 
+  guardarUsuario(user: Usuario): void {
+   localStorage.setItem('usuario_logueado', JSON.stringify(user));
+  }
+
+  cerrarSesion(): void {
+   localStorage.removeItem('token_jwt');
+   localStorage.removeItem('usuario_logueado');
+  }
+
+  estaLogueado(): boolean {
+   return localStorage.getItem('token_jwt') !== null;
+  }
+
+  getToken(): string | null {
+   return localStorage.getItem('token_jwt');
+  }
+
+  getUsuarioLogueado(): Usuario | null {
+   const usuarioJSON = localStorage.getItem('usuario_logueado');
+    
+  if(!usuarioJSON){
+     return null;
+  }
+
+   return JSON.parse(usuarioJSON) as Usuario;
+  }
 }
