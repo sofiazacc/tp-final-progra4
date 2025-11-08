c36cf83285e849235fd17e635dcbde3eba253b80
Nicolas Bernal <nico.k2715@gmail.com>
2025-11-08 12:18:56 -0300

    Creación y diseño funcional  inicial de post-component.ts

---
+++ src/app/components/post-component/post-component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-post',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post-component.html',
+  styleUrls: ['./post-component.css']
+})
+export class PostComponent {
+  @Input() postData: any;
+  liked: boolean = false;
+  
+  getLikesCount(): number {
+    if (!this.postData) return 0;
+    return this.liked ? this.postData.likes + 1 : this.postData.likes;
+  }
+  
+  toggleLike() {
+    this.liked = !this.liked;
+  }
+  
+  guardar() {
+    console.log('Guardar post:', this.postData.id);
+  }
+}
