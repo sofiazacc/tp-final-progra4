f960a9d54acb43bad7be7708cf4c8022c7554826
sofi <sofiazaccagnini04@gmail.com>
2025-11-08 19:12:10 -0300

    Método para registrar al usuario

--- back-simulador/tokenizador.js
+++ back-simulador/tokenizador.js
@@ -9,6 +9,7 @@ const router = jsonServer.router(dbPath);
 //Incorporamos Crypt-js
 const bcrypt = require('bcrypt');
 const { access } = require('fs');
+const { setSourceMapsEnabled } = require('process');
 
 /*Para solucionar errores de CORS, se le da permiso a la interacción
 con este backend simulado y nuestra app de Angular*/
@@ -48,3 +49,39 @@ server.post('/login', (req,res) => {
     }
 });
 
+server.post('/register', (req, res) => {
+    console.log('Servidor recibió en /register:', req.body);
+    const db = router.db;
+    const { nombre, apellido, email, password, rol, username, localidad, provincia } = req.body;
+
+    const salt = bcrypt.genSaltSync(10);
+    const passwordHasheada = bcrypt.hashSync(password, salt);
+
+    const newUser = {
+        id: Math.random().toString(36).substring(2,9),
+        nombre,
+        apellido,
+        email,
+        password: passwordHasheada,
+        rol,
+        nombreDeUsuario: username,
+        localidad,
+        provincia
+    }
+
+    db.get('users').push(newUser).write();
+
+    console.log("Registro exitoso")
+
+    //logueamos al nuevo usuario automáticamente 
+    const token = Buffer.from(`${email}:${Date.now()}`).toString('base64'); 
+    res.status.apply(200).json({accessToken: token, user: user});
+});
+
+server.use(router)
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Bakcend corriendo en http://localhost:${PORT}`);
+    console.log(`Usando la base de datos: ${dbPath}`);
+})
