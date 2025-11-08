d0f73d40c1c52bd5d1eb267af667d0b58653babc
Nicolas Bernal <nico.k2715@gmail.com>
2025-11-08 12:21:53 -0300

    Se creó el servicio feed-service, para su uso en feed. En él, los métodos getPost y getPosts

---
+++ src/app/services/feed-service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { PostModelo } from '../models/postModel';
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedService {
+  
+  
+  url = "http://localhost:3000/posts"
+
+  posts: PostModelo[];
+
+  constructor(private http:HttpClient){
+    this.posts = []
+  }
+
+  getPosts(){
+    return this.http.get<PostModelo[]>(this.url);
+  }
+  
+  getPost(id: string ){
+    return this.http.get<PostModelo>(`${this.url}/${id}`)
+  }
+  
+}
