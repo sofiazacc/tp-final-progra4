6d3021aa4bc9149cf320755cd6379770687eede1
Nicolas Bernal <nico.k2715@gmail.com>
2025-11-08 12:21:03 -0300

    Se  implementó el uso de ngOnInit y se creó la clase obtenerPosts()

--- src/app/pages/feed/feed.ts
+++ src/app/pages/feed/feed.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FeedService } from '../../services/feed-service';
+import { PostComponent } from '../../components/post-component/post-component';
 @Component({
   selector: 'app-feed',
-  imports: [],
+  standalone: true,  
+  imports: [PostComponent],
   templateUrl: './feed.html',
-  styleUrl: './feed.css'
+  styleUrls: ['./feed.css']
 })
-export class Feed {
 
+export class Feed implements OnInit {
+
+  constructor(public feedService: FeedService) {}
+
+  ngOnInit(): void {
+    console.log("Feed iniciado");
+    this.obtenerPosts();
+  }
+  
+  obtenerPosts() {
+    console.log("Ejecutando obtenerPosts()"); 
+    this.feedService.getPosts().subscribe({
+      next: (data) => { 
+        console.log("Datos recibidos:", data);
+        this.feedService.posts = data },
+      error: (e) => console.log(e)
+    });
+  }
 }
