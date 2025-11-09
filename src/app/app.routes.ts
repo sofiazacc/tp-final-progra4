65639b9f6ce75b6eca2d9ebaa7b46ab3d9a7086c
sofi <sofiazaccagnini04@gmail.com>
2025-11-08 23:03:33 -0300

    Se aplicaron los guards en las rutas y se le asignaron los roles que pueden tener acceso a dichas rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,18 +8,20 @@ import { Guardados } from './pages/guardados/guardados';
 import { ArticulosAstronomicos } from './pages/articulos-astronomicos/articulos-astronomicos';
 import { PostsAdmin } from './pages/posts-admin/posts-admin';
 import { UsuariosAdmin } from './pages/usuarios-admin/usuarios-admin';
+
 import { authGuard } from './guards/auth-guard';
+import { rolGuard } from './guards/rol-guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'auth', pathMatch: 'full' },
     {path: '**', redirectTo: 'auth', pathMatch: 'full' },
     {path: 'auth', component: Auth },
     {path: 'home', component: Home },
-    {path: 'feed', component: Feed, canActivate: [authGuard] },
-    {path: 'mapa', component: Mapa },
-    {path: 'perfil', component: Perfil },
-    {path: 'guardados', component: Guardados },
-    {path: 'articulos-astronomicos', component: ArticulosAstronomicos },
-    {path: 'posts-admin', component: PostsAdmin },
-    {path: 'usuarios-admin', component: UsuariosAdmin }
+    {path: 'feed', component: Feed, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
+    {path: 'mapa', component: Mapa, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
+    {path: 'perfil', component: Perfil, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
+    {path: 'guardados', component: Guardados, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
+    {path: 'articulos-astronomicos', component: ArticulosAstronomicos, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
+    {path: 'posts-admin', component: PostsAdmin, canActivate: [authGuard,rolGuard], data: {rol: 'admin'}},
+    {path: 'usuarios-admin', component: UsuariosAdmin, canActivate: [authGuard,rolGuard], data: {rol: 'admin'}}
 ];
