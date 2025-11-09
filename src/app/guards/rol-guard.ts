da619c86cf18b4eb6c1c5c407916acfc8fc06f2c
sofi <sofiazaccagnini04@gmail.com>
2025-11-08 23:02:28 -0300

    Se cambió la lógica del Guards de los roles. Se detecta el rol del usuario y si coincide con el rol especificado en app-router, lo deja pasar, sino, lo redirige según el caso

--- src/app/guards/rol-guard.ts
+++ src/app/guards/rol-guard.ts
@@ -3,19 +3,28 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/authService';
 
 
-export const rolGuard: CanActivateFn = (route, state) => {
+export const rolGuard: CanActivateFn = (route, state) => { 
 
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  const rolEsperado = route.data['rol'];
 
   const usuario = authService.getUsuarioLogueado();
+  const rolUsuario = usuario?.rol;
 
-  if (!usuario || usuario.rol !== 'admin') {
-    console.warn('AuthGuard: Acceso denegado. El usuario no posee permisos para acceder a este endpoint. Redirigiendo a /feed');
-    router.navigate(['/feed']);
+  if(rolUsuario!== rolEsperado){
+    console.warn('AuthGuard: Acceso denegado. El usuario no posee permisos para acceder a este endpoint');
+
+    if(rolUsuario === 'admin'){
+      router.navigate(['/admin/usuarios']);
+    }else if(rolUsuario === 'fotografo'){
+      router.navigate(['feed']);
+    }else{
+      router.navigate(['/auth']);
+    }
     return false
   }
-  
+
   return true;
 };
