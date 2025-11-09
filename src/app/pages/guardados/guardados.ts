27c5bbfb7c3ea5be7b4ad2e6c5a7df9be8bf1dcd
sofi <sofiazaccagnini04@gmail.com>
2025-11-08 21:57:53 -0300

    Generando componentes para crear las páginas de navegación correspondiente a cada tipo de usuario. Empieza la implementación de guards

---
+++ src/app/pages/guardados/guardados.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guardados',
+  imports: [],
+  templateUrl: './guardados.html',
+  styleUrl: './guardados.css',
+})
+export class Guardados {
+
+}
