f76e2dae7d6812845f4f4e02faf2b9a9d7cba358
Nicolas <nico.k2715@gmail.com>
2025-11-20 18:16:06 -0300

    Se agregó el Header y el Footer en el array de los imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuGeneral } from './components/menu-general/menu-general';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuGeneral],
+  imports: [RouterOutlet, MenuGeneral, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
