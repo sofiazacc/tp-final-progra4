be20a0fb292699b894809b1db88801b79cc3fd5f
Nicolas Bernal <nico.k2715@gmail.com>
2025-11-20 14:48:57 -0300

    Se creó y comenzó el desarrollo de la calculadora del 500

---
+++ src/app/components/caculadora500-component/caculadora500-component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Calculadora500 } from '../../services/calculadora500';
+import { FormBuilder} from '@angular/forms';
+
+@Component({
+  selector: 'app-caculadora500-component',
+  imports: [],
+  templateUrl: './caculadora500-component.html',
+  styleUrl: './caculadora500-component.css'
+})
+export class Caculadora500Component {
+
+  constructor(private calculadora500: Calculadora500, private fb: FormBuilder) { 
+
+    
+
+  }
+
+  }
