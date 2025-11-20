f590062cfff482eb6a4215c99acb8333c8382aeb
Nicolas Bernal <nico.k2715@gmail.com>
2025-11-20 14:48:00 -0300

    Se creó y comenzó el desarrollo de la calculadora de horas mágicas

---
+++ src/app/components/caculadora-horas-magicas-component/caculadora-horas-magicas-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caculadora-horas-magicas-component',
+  imports: [],
+  templateUrl: './caculadora-horas-magicas-component.html',
+  styleUrl: './caculadora-horas-magicas-component.css'
+})
+export class CaculadoraHorasMagicasComponent {
+
+}
