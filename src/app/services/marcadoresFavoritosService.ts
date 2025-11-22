eed0ef16b615fa27b85c711e201d2f5abb414b49
sofi <sofiazaccagnini04@gmail.com>
2025-11-22 20:47:59 -0300

    Creé marcadoresFavoritosService: -Método esFavorito: Chequea si este marcador ya está guardado en favoritos. -Método para amrcar o desemarcar favoritos: Chequea si ya está guardado, si es así, se quita, sino, se guarda. Una vez realizado el chequeo, guardamos los datos actualizados del fotógrafo, tanto en la BDD como en el localstorage (por si las dudas)

---
+++ src/app/services/marcadoresFavoritosService.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from './authService';
+import { Fotografo } from '../models/usuario';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoritosService {
+  constructor(private http: HttpClient, private usuarioService: AuthService) {}
+
+  private usuarios = 'http://localhost:3000/usuarios';
+
+  //Verificamos si es favorito
+  esFavorito(marcadorID: string):boolean {
+    const usuarioActual = this.usuarioService.getUsuarioLogueado() as Fotografo;
+    return usuarioActual.marcadoresGuardadosID?.includes(marcadorID) || false;
+  }
+
+//Marcar o desmarcar como favorito
+  marcarDesmarcarComoFavorito(marcadorID: string): Observable<Fotografo> {
+    const usuarioActual = this.usuarioService.getUsuarioLogueado() as Fotografo;
+
+    let marcadoresGuardados= usuarioActual.marcadoresGuardadosID || [];
+
+    //si ya está guardado, lo quitamos; si no, lo agregamos
+    if(this.esFavorito(marcadorID)){
+      marcadoresGuardados = marcadoresGuardados.filter(id => id !== marcadorID);
+    } else {
+      marcadoresGuardados.push(marcadorID);
+    }
+
+    const datosActualizados = { marcadoresGuardadosID: marcadoresGuardados };
+
+    return this.http.put<Fotografo>(`${this.usuarios}/${usuarioActual.id}`, datosActualizados).pipe(
+      //Actualizamos el usuario en el localStorage
+
+      tap(() => {
+        const usuarioActualizadoLocal = { ...usuarioActual, ...datosActualizados };
+        this.usuarioService.guardarUsuario(usuarioActualizadoLocal as Fotografo);
+      })
+    );
+  }
+
+}
