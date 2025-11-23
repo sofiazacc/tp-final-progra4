bf4c5b72475e0ba7082f4bc8e04b3266d86f8189
sofi <sofiazaccagnini04@gmail.com>
2025-11-23 17:40:37 -0300

    Perfeccionando animacuiones ;:D

--- src/app/components/evento-astronomico/evento-astronomico.ts
+++ src/app/components/evento-astronomico/evento-astronomico.ts
@@ -1,10 +1,10 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-evento-astronomico',
-  imports: [RouterLink, DatePipe],
+  imports: [DatePipe],
   templateUrl: './evento-astronomico.html',
   styleUrl: './evento-astronomico.css',
 })
@@ -18,16 +18,20 @@ export class EventoAstronomicoComponente {
     const fechaInicio = this.evento.fecha_inicio || this.evento.fechaInicio;
     const fechaFin = this.evento.fecha_fin || this.evento.fechaFin;
   
-    const inicio = new Date(fechaInicio).toISOString().replace(/-|:|\.\d{3}/g, '');
-    const fin = new Date(fechaFin).toISOString().replace(/-|:|\.\d{3}/g, '');
+    const inicio = new Date(fechaInicio + 'T03:00:00Z').toISOString().replace(/-|:|\.\d{3}/g, '');
+    const fin = new Date(fechaFin + 'T03:00:00Z' ).toISOString().replace(/-|:|\.\d{3}/g, '');
   
     return `https://www.google.com/calendar/render?action=TEMPLATE&text=${titulo}&details=${descripcion}&dates=${inicio}/${fin}`;
   }
-  
+
   comprobarSiEsEventoActual(): boolean {
     const ahora = new Date();
-    const inicio = new Date(this.evento.fechaInicio);
-    const fin = new Date(this.evento.fechaFin);
+    const inicio = new Date(this.evento.fecha_inicio  + 'T03:00:00Z');
+    const fin = new Date(this.evento.fecha_fin  + 'T03:00:00Z');
     return ahora >= inicio && ahora <= fin;
+  }
+
+  abrirCalendar(): void {
+  window.open(this.generarLinkACalendar(), '_blank');
   }
 }
