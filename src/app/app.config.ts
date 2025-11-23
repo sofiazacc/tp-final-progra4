4d54a0acf7a5fb15515cb110c2dfe232922f2abb
sofi <sofiazaccagnini04@gmail.com>
2025-11-23 19:34:03 -0300

    Una opequeña configuración para que las fechas aprezcan en español

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, LOCALE_ID } from '@angular/core'; // <--- 1. IMPORTAR LOCALE_ID
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -6,11 +6,18 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token-interceptor';
 import { errorInterceptor } from './interceptors/error-interceptor';
 
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor, errorInterceptor]))
+    provideHttpClient(withInterceptors([tokenInterceptor, errorInterceptor])),
+    
+    { provide: LOCALE_ID, useValue: 'es' } 
   ]
 };
