bc4a8e81f94d99ac1e23460f81ba63a2a00c7165
Nicolas <nico.k2715@gmail.com>
2025-11-23 17:18:50 -0300

    Se modificó el modelo para portar los 10 datos necesarios

--- src/app/models/horasMagicasModel.ts
+++ src/app/models/horasMagicasModel.ts
@@ -1,12 +1,16 @@
-export interface HorasMagicasModel{
-    amanecer?: Date | null;
-    atardecer?: Date | null;
-    inicioHoraAzul?: Date | null;
-    finHoraAzul?: Date | null;
-    inicioHoraDorara?: Date | null;
-    finHoraDorada?: Date | null;
-    
+export interface HorasMagicasModel {
+  amanecer: Date;
+  atardecer: Date;
+  horaAzulAM_Inicio: Date;
+  horaAzulAM_Fin: Date;
+  horaAzulPM_Inicio: Date;
+  horaAzulPM_Fin: Date;
+  horaDoradaAM_Inicio: Date;
+  horaDoradaAM_Fin: Date;
+  horaDoradaPM_Inicio: Date;
+  horaDoradaPM_Fin: Date;
 }
+
 
 // No es necesario que los atributos coincidan con los campos del json. El mapeo
 // se lleva a cabo en el service.
