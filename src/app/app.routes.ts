85334eb8774c11e7805f9a0da1eab5e312cab624
Nicolas <nico.k2715@gmail.com>
2025-11-23 17:15:10 -0300

    Se agregaron las dos rutas a las calculadoras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Auth } from './pages/auth/auth';
 import { Home } from './pages/home/home';
 import { Feed } from './pages/feed/feed'; 
 import { authGuard } from './guards/auth-guard';
+import { Caculadora500Component } from './components/caculadora500-component/caculadora500-component';
+import { CaculadoraHorasMagicasComponent } from './components/caculadora-horas-magicas-component/caculadora-horas-magicas-component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'auth', component: Auth },
     {path: 'home', component: Home },
-    {path: 'feed', component: Feed, canActivate: [authGuard] }
+    {path: 'calculators', component: Caculadora500Component},
+    {path: 'calculators2', component: CaculadoraHorasMagicasComponent},
+    {path: 'feed', component: Feed, canActivate: [authGuard] },
 ];
