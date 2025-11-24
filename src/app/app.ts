cb318d59e64a6c91ee6ffa588587746a180cdf14
sofi <sofiazaccagnini04@gmail.com>
2025-11-24 00:42:16 -0300

    Se agregaron animaciones de scrolleo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('phost');
+
+  ngOnInit(){
+    AOS.init({
+      duration: 800,
+      easing: 'ease-in-out', 
+      once: true,
+      mirror: false, 
+      offset: 120,
+    });
+  }
 }
+
+
