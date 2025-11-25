300da8dfa752ecd66e207ab7582bd4d20061bc28
sofi <sofiazaccagnini04@gmail.com>
2025-11-25 07:20:46 -0300

    dependencias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,9 @@ import { Guardados } from './pages/guardados/guardados';
 import { EventosAstronomicos } from './pages/eventos-astronomicos/eventos-astronomicos';
 import { PostsAdmin } from './pages/posts-admin/posts-admin';
 import { UsuariosAdmin } from './pages/usuarios-admin/usuarios-admin';
-
+import { Caculadora500Component } from './components/caculadora500-component/caculadora500-component';
+import { CaculadoraHorasMagicasComponent } from './components/caculadora-horas-magicas-component/caculadora-horas-magicas-component';
+import { PosicionViaLacteaComponent } from './components/posicion-via-lactea/posicion-via-lactea';
 import { authGuard } from './guards/auth-guard';
 import { rolGuard } from './guards/rol-guard';
 
@@ -22,5 +24,8 @@ export const routes: Routes = [
     {path: 'guardados', component: Guardados, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
     {path: 'eventos', component: EventosAstronomicos, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
     {path: 'posts-admin', component: PostsAdmin, canActivate: [authGuard,rolGuard], data: {rol: 'admin'}},
-    {path: 'usuarios-admin', component: UsuariosAdmin, canActivate: [authGuard,rolGuard], data: {rol: 'admin'}}
+    {path: 'usuarios-admin', component: UsuariosAdmin, canActivate: [authGuard,rolGuard], data: {rol: 'admin'}},
+    {path: 'calculatora', component: Caculadora500Component, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
+    {path: 'calculatora2', component: CaculadoraHorasMagicasComponent, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}},
+    {path: 'viaLactea', component: PosicionViaLacteaComponent, canActivate: [authGuard,rolGuard], data: {rol: 'fotografo'}}
 ];
