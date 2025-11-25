4f009b0ebd79a8ca0bb82d23c4e4d5d22acb62a3
Nicolas <nico.k2715@gmail.com>
2025-11-25 06:34:25 -0300

    Se crearon los componentes pertenecientes a modificar perfil

---
+++ src/app/components/eliminar-cuenta/eliminar-cuenta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-cuenta',
+  imports: [],
+  templateUrl: './eliminar-cuenta.html',
+  styleUrl: './eliminar-cuenta.css',
+})
+export class EliminarCuenta {
+
+}
