2688ca29d7fe5953339598a0e723a6d766254c9a
sofi <sofiazaccagnini04@gmail.com>
2025-11-25 07:33:58 -0300

    Adiciones del cherry-pcik

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuGeneral } from './components/menu-general/menu-general';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+
 import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],  
+  imports: [RouterOutlet, MenuGeneral, Header, Footer],  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
