2b59e6c2cee2cd552576fb07b58c19b63342d97e
Nicolas <nico.k2715@gmail.com>
2025-11-26 17:53:13 -0300

    menu

--- src/app/components/menu-general/menu-general.ts
+++ src/app/components/menu-general/menu-general.ts
@@ -21,16 +21,17 @@ export class MenuGeneral {
 
   itemBoton = {icon: 'thumbnail_bar', label: 'Desplegar', route: ''}
 
-  menuItems = [
+  menuItems = 
+  [
     {icon: 'dynamic_feed', label: 'Feed', route: 'feed'},
     {icon: 'map_search', label: 'Mapa', route: 'mapa'},
     {icon: 'person', label: 'Perfil', route: 'perfil'},
     {icon: 'bookmark', label: 'Posts guardados', route: 'guardados'},
     {icon: 'account_circle_off', label: 'Cerrar sesión', route: 'auth'},
-    {icon: 'thumbnail_bar', label: 'Eventos', route: 'eventos'},
-    {icon: 'thumbnail_bar', label: 'Regla del 500', route: 'calculadora'},
-    {icon: 'thumbnail_bar', label: 'Horas mágicas', route: 'calculadora2'},
-    {icon: 'thumbnail_bar', label: 'Vía Láctea', route: 'viaLactea'}
+    {icon: 'orbit', label: 'Eventos', route: 'eventos'},
+    {icon: 'calculate', label: 'Regla del 500', route: 'calculadora'},
+    {icon: 'wb_twilight', label: 'Horas mágicas', route: 'calculadora2'},
+    {icon: 'planet', label: 'Vía Láctea', route: 'viaLactea'}
   ]
 
   verificarAccion(item: any) {
