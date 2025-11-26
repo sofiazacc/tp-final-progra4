7bd4d7f4f02d1aef18536022431693c1cd42bb39
Nicolas <nico.k2715@gmail.com>
2025-11-26 14:55:50 -0300

    Se implementó el uso de un arreglo de posts, para mostrarlos en el feed

--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -2,23 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/authService';
 import { Fotografo } from '../../models/usuario';
 import { ModalEditarPerfil } from '../../components/modal-editar-perfil/modal-editar-perfil';
+import { FeedService } from '../../services/feedService';
+import { PostModelo } from '../../models/post';
+import { PostComponent } from '../../components/post-component/post-component';
 
 
 @Component({
   selector: 'app-perfil',
-  imports: [ModalEditarPerfil],
+  imports: [ModalEditarPerfil, PostComponent],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
-export class Perfil{
+export class Perfil implements OnInit{
 
   fotografo: Fotografo | null
   estadoModal: boolean = false
+  idFotografo: string | undefined
+  posts: PostModelo[] = []
 
-  constructor(public authService: AuthService){
+  constructor(
+    public authService: AuthService,
+    public feedService: FeedService
+  ){
     this.fotografo = this.authService.getfotografoActual()
     console.log("Datos en LocalStorage:", localStorage.getItem('usuario_logueado'));
     console.log("Fotógrafo parseado:", this.fotografo);
+  }
+
+  ngOnInit(): void {
+    this.obtenerUsuario();
   }
 
   modificarPerfil(){
@@ -28,6 +40,29 @@ export class Perfil{
   cerrarModal(){
     this.estadoModal = false
     this.fotografo = this.authService.getfotografoActual();
+  }
+
+  obtenerUsuario(){
+    const usuarioLogueado = this.authService.getfotografoActual();
+
+    if (usuarioLogueado) {
+      this.fotografo = usuarioLogueado;
+      
+      this.idFotografo = usuarioLogueado.id.toString();
+
+      console.log("Fotógrafo activo:", this.fotografo);
+
+      this.feedService.getPostsByFotografoId(this.idFotografo).subscribe({
+        next: (data) => {
+          this.posts = data;
+          console.log("Posts cargados:", this.posts);
+        },
+        error: (e) => console.error("Error cargando posts:", e)
+      });
+
+    } else {
+      console.error("No hay un usuario logueado");
+    }
   }
 
 }
